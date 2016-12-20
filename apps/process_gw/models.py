9f700a2c8668a306c2fff1f60896912cf1d8b2d9
veris-neerajdhiman <neeraj.dhiman@veris.in>
2016-12-20 19:39:16 +0530

    Process session models

--- apps/process_gw/models.py
+++ apps/process_gw/models.py
@@ -1,3 +1,49 @@
+
+from django.contrib import admin
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+import uuid
+
+
+REQUEST_STATES = (
+        ('init', 'Initialize'),
+        ('inprocess', 'In Process'),
+        ('wait', 'Waiting'),
+        ('succeeded', 'Completed'),
+        ('failed', 'Failed'),
+    )
+
+class RequestSession(models.Model):
+    """
+    """
+    reuestId = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    state = models.CharField(_('current state of a process'), max_length=20, choices=REQUEST_STATES)
+
+    def __str__(self):
+        return self.reuestId
+
+class RequestTrace(models.Model):
+    """
+    """
+    request = models.ForeignKey(RequestSession)
+    state = models.CharField(_('state of a process'), max_length=20, choices=REQUEST_STATES)
+    payload = JSONField(_('widgets config, for internal use'))
+    response = JSONField(_('widgets config, for internal use'))
+
+    def __str__(self):
+        return self.request
+
+class RequestSessionAdmin(admin.ModelAdmin):
+    list_display = ('reuestId', 'state', )
+    list_display_links = ('reuestId', )
+
+class RequestTraceAdmin(admin.ModelAdmin):
+    list_display = ('request', 'state', )
+    list_display_links = ('request', )
+
+admin.site.register(RequestSession, RequestSessionAdmin)
+admin.site.register(RequestTrace, RequestTraceAdmin)
