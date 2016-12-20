1ea9ae05f4c31bfaf16b73c76fe7c3f4b4b71f39
veris-neerajdhiman <neeraj.dhiman@veris.in>
2016-12-20 18:36:50 +0530

    Add : serviuce we will  integrate comes here

---
+++ apps/api_gw/libs/urls.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+# future
+from __future__ import unicode_literals
+
+# Django
+from django.conf.urls import url, include
+from django.conf import settings
+
+
+urlpatterns = [
+        
+        url(r'', include('apps.api_gw.libs.hunger.routers')),   
+        
+
+]
+
+
