f3ed2b507c1f4f477a20afbe93521f356778cee2
veris-neerajdhiman <neeraj.dhiman@veris.in>
2016-12-20 17:43:46 +0530

    Add : widget models

--- apps/api_gw/widgets/models.py
+++ apps/api_gw/widgets/models.py
@@ -1,3 +1,23 @@
+from django.contrib import admin
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+
+class Widgets(models.Model):
+    """
+    """
+    name = models.CharField(_('widget name'), max_length=100)
+    config = JSONField(_('widgets config, for internal use'))
+
+    def __str__(self):
+        return self.name
+
+class WidgetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', )
+    list_display_links = ('name', )
+
+
+admin.site.register(Widgets, WidgetAdmin)
